03cbe80d6bde03bc327ed1e5d0ffbcee7cac9f65
xterminal86 <xterminal86@gmail.com>
2015-09-21 00:46:41 +0300

    "Binary Tree" adaptation and comments

--- Assets/scripts/dungeon_generator/BinaryTree.cs
+++ Assets/scripts/dungeon_generator/BinaryTree.cs
@@ -4,6 +4,20 @@ using System.Collections.Generic;
 
 public class BinaryTree : GenerationAlgorithmBase
 {  
+  /// <summary>
+  /// Adaptation of "Binary Tree" algorithm (from e-book "Mazes for Programmers" by Jamis Buck, ISBN-13: 978-1-68050-055-4, p. 6)
+  /// 
+  /// General description given in the book:
+  /// 1) Starting from random unvisited point, decide to carve north or east
+  /// 2) Mark current cell as visited
+  /// 3) Repeat until all cells are visited
+  /// 
+  /// Algorithm described in the book was designed for "paper sheet walls" grid (look for comments in GrowingTree.cs), 
+  /// so I had to lock walls to get more or less similar behaviour.
+  /// This particular implementation of the algorithm carves right or down (i.e. east or south) as opposed to north or east.
+  /// 
+  /// </summary>
+  /// <param name="grid"></param>
   public override void Do(Grid grid)
   {
     Int2 tmp = new Int2();
@@ -24,27 +38,29 @@ public class BinaryTree : GenerationAlgorithmBase
         // Carve to the right
         if (val == 0)
         {
-          grid.Map[x, y + 1].CellType = CellType.FLOOR;          
-
-          grid.Map[x - 1, y - 1].Status = CellStatus.LOCKED;
-          grid.Map[x - 1, y].Status = CellStatus.LOCKED;
-          grid.Map[x - 1, y + 1].Status = CellStatus.LOCKED;
-          grid.Map[x, y - 1].Status = CellStatus.LOCKED;
-          grid.Map[x + 1, y - 1].Status = CellStatus.LOCKED;
+          grid.Map[x, y + 1].CellType = CellType.FLOOR;
+          // Lock down
           grid.Map[x + 1, y].Status = CellStatus.LOCKED;
         }
+        // Carve down
         else
         {
           grid.Map[x + 1, y].CellType = CellType.FLOOR;          
-
-          grid.Map[x - 1, y - 1].Status = CellStatus.LOCKED;
-          grid.Map[x - 1, y].Status = CellStatus.LOCKED;
-          grid.Map[x - 1, y + 1].Status = CellStatus.LOCKED;
-          grid.Map[x, y - 1].Status = CellStatus.LOCKED;
-          grid.Map[x, y + 1].Status = CellStatus.LOCKED;
-          grid.Map[x + 1, y - 1].Status = CellStatus.LOCKED;          
+          // Lock right
+          grid.Map[x, y + 1].Status = CellStatus.LOCKED;          
         }
       }
-    }    
+    }
+    
+    // Erase last column and row before maze boundary to avoid getting unreachable zones in the maze
+    for (int i = 1; i < grid.MapHeight - 1; i++)
+    {
+      grid.Map[i, grid.MapWidth - 2].CellType = CellType.FLOOR;
+    }
+
+    for (int i = 1; i < grid.MapWidth - 1; i++)
+    {
+      grid.Map[grid.MapHeight - 2, i].CellType = CellType.FLOOR;
+    }
   }  
 }
