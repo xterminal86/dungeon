8c4a19c5def4329267c6a93fa7146043b6f8030a
xterminal86 <xterminal86@gmail.com>
2015-09-20 18:45:01 +0300

    "Growing Tree" implementation

--- Assets/scripts/dungeon_generator/GrowingTree.cs
+++ Assets/scripts/dungeon_generator/GrowingTree.cs
@@ -7,8 +7,7 @@ public class GrowingTree : GenerationAlgorithmBase
   Grid _gridRef;
 
   List<Int2> _visitedCells = new List<Int2>();
-  List<Int2> _untouchableWalls = new List<Int2>();
-
+  
   int _safeguard = 0;
 
   bool _gtRandomFlag = false;
@@ -24,10 +23,12 @@ public class GrowingTree : GenerationAlgorithmBase
     Cell c = grid.GetRandomCell();
 
     Int2 pos = new Int2((int)c.Coordinates.x, (int)c.Coordinates.y);
+    pos.X = 8;
+    pos.Y = 3;
     grid.Map[pos.X, pos.Y].Status = CellStatus.VISITED;
     _visitedCells.Add(pos);
 
-    Debug.Log("Starting point: " + pos);
+    //Debug.Log("Starting point: " + pos);
 
     MakeWall(pos);
 
@@ -50,15 +51,15 @@ public class GrowingTree : GenerationAlgorithmBase
         pos = _visitedCells[_visitedCells.Count - 1];
       }
 
-      Debug.Log("Current cell " + pos);
+      //Debug.Log("Current cell " + pos);
 
-      neighbour = ChooseNeighbour(pos);
+      neighbour = ChooseNeighbour(pos);            
       if (neighbour == null)
       {
         RemoveCell(pos);
       }
       else
-      {
+      {        
         MakeWall(neighbour);
         _gridRef.Map[neighbour.X, neighbour.Y].Status = CellStatus.VISITED;
         _visitedCells.Add(neighbour);
@@ -69,92 +70,6 @@ public class GrowingTree : GenerationAlgorithmBase
     }
   }
 
-#if false
-  public override void Do(Grid grid)
-  {
-    _gridRef = grid;
-
-    //CreateBounds();
-
-    Cell c = grid.GetRandomCell();
-  
-    Int2 pos = new Int2((int)c.Coordinates.x, (int)c.Coordinates.y);
-    _visitedCells.Add(pos);
-
-    MakeWall(pos);
-
-    Int2 neighbour = new Int2();
-
-    //Int2 neighbour = ChooseNeighbour(pos);
-    //_visitedCells.Add(neighbour);
-    //InvalidatePreviousCell(pos, neighbour);
-
-    string debugString = string.Empty;
-
-    while (_visitedCells.Count != 0)
-    {
-      if (_safeguard > 100000)
-      {
-        Debug.LogWarning("Terminated by safeguard on " + pos);
-        break;
-      }
-
-      if (_gtRandomFlag)
-      {
-        int index = Random.Range(0, _visitedCells.Count);
-        pos = _visitedCells[index];
-
-        neighbour = ChooseNeighbour(pos);
-        if (neighbour == null)
-        {
-          RemoveCell(pos);
-        }
-        else
-        {
-          MakeWall(neighbour);
-          _visitedCells.Add(neighbour);
-          InvalidatePreviousCell(pos, neighbour);
-        }
-      }
-      else
-      {        
-        pos = _visitedCells[_visitedCells.Count - 1];
-
-        //Debug.Log("Current cell " + pos);
-
-        neighbour = ChooseNeighbour(pos);
-        if (neighbour == null)
-        {          
-          RemoveCell(pos);
-        }
-        else
-        {
-          MakeWall(neighbour);          
-          _visitedCells.Add(neighbour);
-          InvalidatePreviousCell(pos, neighbour);
-        }
-
-        /*
-        debugString = string.Empty;
-
-        debugString += string.Format("Visited cells ({0}) => ", _visitedCells.Count);
-
-        foreach (var item in _visitedCells)
-        {
-          debugString += string.Format("{0}", item);
-        }
-
-        Debug.Log(debugString);
-        */
-
-        //Debug.Log(pos + " " + neighbour);
-      }
-
-      _safeguard++;
-    }    
-  }  
-#endif
-
   void CreateBounds()
   {
     Int2 cell = new Int2();
@@ -163,12 +78,12 @@ public class GrowingTree : GenerationAlgorithmBase
       cell.X = 0;
       cell.Y = i;
       _gridRef.Map[0, i].CellType = CellType.WALL;
-      _untouchableWalls.Add(cell);
+      _gridRef.Map[0, i].Status = CellStatus.LOCKED;
 
       cell.X = _gridRef.MapHeight - 1;
       cell.Y = i;
       _gridRef.Map[_gridRef.MapHeight - 1, i].CellType = CellType.WALL;
-      _untouchableWalls.Add(cell);
+      _gridRef.Map[_gridRef.MapHeight - 1, i].Status = CellStatus.LOCKED;
     }
 
     for (int i = 1; i < _gridRef.MapHeight; i++)
@@ -176,12 +91,12 @@ public class GrowingTree : GenerationAlgorithmBase
       cell.X = i;
       cell.Y = 0;
       _gridRef.Map[i, 0].CellType = CellType.WALL;
-      _untouchableWalls.Add(cell);
+      _gridRef.Map[i, 0].Status = CellStatus.LOCKED;
 
       cell.X = i;
       cell.Y = _gridRef.MapWidth - 1;
       _gridRef.Map[i, _gridRef.MapWidth - 1].CellType = CellType.WALL;
-      _untouchableWalls.Add(cell);
+      _gridRef.Map[i, _gridRef.MapWidth - 1].Status = CellStatus.LOCKED;      
     }
   }
 
@@ -198,9 +113,9 @@ public class GrowingTree : GenerationAlgorithmBase
 
       if (_gridRef.Map[prevPos.X - 1, prevPos.Y].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X - 1, prevPos.Y].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X + 1, prevPos.Y].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X + 1, prevPos.Y].Status = CellStatus.LOCKED; }
-      if (_gridRef.Map[prevPos.X - 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X - 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X, prevPos.Y + 1].Status = CellStatus.LOCKED; }
-      if (_gridRef.Map[prevPos.X + 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X + 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
     }
     else if (!leftPassage)
     {
@@ -210,9 +125,9 @@ public class GrowingTree : GenerationAlgorithmBase
 
       if (_gridRef.Map[prevPos.X - 1, prevPos.Y].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X - 1, prevPos.Y].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X + 1, prevPos.Y].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X + 1, prevPos.Y].Status = CellStatus.LOCKED; }
-      if (_gridRef.Map[prevPos.X - 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X - 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X, prevPos.Y - 1].Status = CellStatus.LOCKED; }
-      if (_gridRef.Map[prevPos.X + 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X + 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
     }
     else if (downPassage)
     {
@@ -221,9 +136,9 @@ public class GrowingTree : GenerationAlgorithmBase
       // #.#
       // ###
 
-      if (_gridRef.Map[prevPos.X - 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X - 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X - 1, prevPos.Y].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y].Status = CellStatus.LOCKED; }
-      if (_gridRef.Map[prevPos.X - 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X - 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X - 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X, prevPos.Y - 1].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X, prevPos.Y - 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X, prevPos.Y + 1].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X, prevPos.Y + 1].Status = CellStatus.LOCKED; }
     }
@@ -234,39 +149,12 @@ public class GrowingTree : GenerationAlgorithmBase
       // X.X
       // XXX
 
-      if (_gridRef.Map[prevPos.X + 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X + 1, prevPos.Y - 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y - 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X + 1, prevPos.Y].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y].Status = CellStatus.LOCKED; }
-      if (_gridRef.Map[prevPos.X + 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
+      //if (_gridRef.Map[prevPos.X + 1, prevPos.Y + 1].CellType == CellType.WALL) { _gridRef.Map[prevPos.X + 1, prevPos.Y + 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X, prevPos.Y - 1].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X, prevPos.Y - 1].Status = CellStatus.LOCKED; }
       if (_gridRef.Map[prevPos.X, prevPos.Y + 1].CellType == CellType.WALL)     { _gridRef.Map[prevPos.X, prevPos.Y + 1].Status = CellStatus.LOCKED; }
-    }
-
-    /*
-    int xMin = prevPos.X - 1;
-    int xMax = prevPos.X + 1;
-    int yMin = prevPos.Y - 1;
-    int yMax = prevPos.Y + 1;
-
-    xMin = Mathf.Clamp(xMin, 0, _gridRef.MapHeight - 1);
-    xMax = Mathf.Clamp(xMax, 0, _gridRef.MapHeight - 1);
-    yMin = Mathf.Clamp(yMin, 0, _gridRef.MapWidth - 1);
-    yMax = Mathf.Clamp(yMax, 0, _gridRef.MapWidth - 1);
-
-    Int2 tmp = new Int2();
-    for (int x = xMin; x <= xMax; x++)
-    {
-      for (int y = yMin; y <= yMax; y++)
-      {
-        tmp.X = x;
-        tmp.Y = y;
-                
-        if (_gridRef.Map[x, y].CellType == CellType.WALL)
-        {
-          _gridRef.Map[x, y].Status = CellStatus.LOCKED;
-        }
-      }
-    }
-    */
+    }    
   }
 
   void RemoveCell(Int2 pos)
@@ -315,34 +203,7 @@ public class GrowingTree : GenerationAlgorithmBase
           _gridRef.Map[i, j].CellType = CellType.WALL;
         }
       }
-    }
-
-    /*
-    Int2 ul = new Int2(xMin, yMin);
-    Int2 ur = new Int2(xMax, yMin);
-    Int2 bl = new Int2(xMin, yMax);
-    Int2 br = new Int2(xMax, yMax);
-
-    if (!DoesCellExist(ul, _untouchableWalls))
-    {
-      _untouchableWalls.Add(ul);
-    }
-
-    if (!DoesCellExist(ur, _untouchableWalls))
-    {
-      _untouchableWalls.Add(ur);
-    }
-
-    if (!DoesCellExist(bl, _untouchableWalls))
-    {
-      _untouchableWalls.Add(bl);
-    }
-
-    if (!DoesCellExist(br, _untouchableWalls))
-    {
-      _untouchableWalls.Add(br);
-    }
-    */
+    }    
   }
 
   List<Int2> _neighbours = new List<Int2>();
@@ -357,41 +218,27 @@ public class GrowingTree : GenerationAlgorithmBase
     Int2 left = new Int2(center.X, center.Y - 1);
     Int2 right = new Int2(center.X, center.Y + 1);
 
-    Int2 tmp = new Int2();
-
     res = IsCellValid(up);
     if (res)
     {
-      tmp.X = center.X - 1;
-      tmp.Y = center.Y;
-
       _neighbours.Add(up);
     }
 
     res = IsCellValid(down);
     if (res)
     {
-      tmp.X = center.X + 1;
-      tmp.Y = center.Y;
-
       _neighbours.Add(down);
     }
 
     res = IsCellValid(left);
     if (res)
     {
-      tmp.X = center.X;
-      tmp.Y = center.Y - 1;
-
       _neighbours.Add(left);
     }
 
     res = IsCellValid(right);
     if (res)
     {
-      tmp.X = center.X;
-      tmp.Y = center.Y + 1;
-
       _neighbours.Add(right);
     }
 
@@ -422,19 +269,7 @@ public class GrowingTree : GenerationAlgorithmBase
     if (_gridRef.Map[x, y].Status == CellStatus.VISITED || _gridRef.Map[x, y].Status == CellStatus.LOCKED)
     {
       return false;
-    }
-
-    /*
-    if (DoesCellExist(pos, _untouchableWalls))
-    {
-      return false;
     }    
-    
-    if (DoesCellExist(pos, _visitedCells))
-    {
-      return false;
-    }
-    */
 
     return true;
   }
