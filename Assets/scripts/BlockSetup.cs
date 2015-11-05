03e15feb10eeb34d71913b4ea051805a9faf5cc9
xterminal86 <xterminal86@gmail.com>
2015-11-05 12:00:43 +0300

    custom editor for automatic texture assignment to blocks

---
+++ Assets/scripts/BlockSetup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class BlockSetup : MonoBehaviour 
+{
+  public GameObject Quad1;
+  public GameObject Quad2;
+  public GameObject Quad3;
+  public GameObject Quad4;
+  public GameObject Quad5;
+  public GameObject Quad6;
+
+  public Material DragMaterialHere;
+
+  void Update()
+  {
+    if (DragMaterialHere != null)
+    {
+      Quad1.renderer.material = DragMaterialHere;
+      Quad2.renderer.material = DragMaterialHere;
+      Quad3.renderer.material = DragMaterialHere;
+      Quad4.renderer.material = DragMaterialHere;
+      Quad5.renderer.material = DragMaterialHere;
+      Quad6.renderer.material = DragMaterialHere;
+    }
+  }
+}
---
+++ Assets/scripts/Editor/BlockSetupInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(BlockSetup))]
+public class BlockSetupInspector : Editor
+{
+  public override void OnInspectorGUI()
+  {
+    //base.OnInspectorGUI();
+
+    BlockSetup bs = (BlockSetup)target;
+
+    GUI.changed = false;
+
+    bs.DragMaterialHere = (Material)EditorGUILayout.ObjectField("Drag Material Here", bs.DragMaterialHere, typeof(Material));
+
+    if (GUI.changed)
+    {
+      EditorUtility.SetDirty(bs);
+    }
+  }
+}
