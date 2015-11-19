96c2cc6dc2fc27a880b30c3233d6eb7a5ab0386e
xterminal86 <xterminal86@gmail.com>
2015-11-19 16:12:10 +0300

    prefabs manager custom inspector for automatic generation of prefabs list for instantiation

---
+++ Assets/scripts/Editor/PrefabsManagerInspector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+[CustomEditor(typeof(PrefabsManager))]
+public class PrefabsManagerInspector : Editor 
+{
+  public override void OnInspectorGUI()
+  {
+    PrefabsManager pm = target as PrefabsManager;
+
+    if (pm == null) return;
+
+    string[] paths = { "Assets/prefabs-release" };
+
+    DrawDefaultInspector();
+
+    if (GUILayout.Button("Generate List"))
+    {
+      pm.Prefabs.Clear();
+
+      string[] dirs = Directory.GetDirectories("Assets/prefabs-release", "*", SearchOption.AllDirectories);
+      for (int i = 0; i < dirs.Length; i++)
+      {
+        string[] array = Directory.GetFiles(dirs[i], "*.prefab");
+
+        for (int j = 0; j < array.Length; j++)
+        {
+          GameObject o = AssetDatabase.LoadAssetAtPath(array[j], typeof(GameObject)) as GameObject;
+          pm.Prefabs.Add(o);
+        }
+      }
+    }
+  }
+
+}
