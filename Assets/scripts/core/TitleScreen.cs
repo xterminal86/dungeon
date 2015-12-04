913ac105eb3bb959ecb12baf5e77275ae1d36b35
xterminal86 <xterminal86@gmail.com>
2015-12-04 17:53:32 +0300

    stairs model title screen improvements

--- Assets/scripts/core/TitleScreen.cs
+++ Assets/scripts/core/TitleScreen.cs
@@ -4,6 +4,51 @@ using System.Collections.Generic;
 
 public class TitleScreen : MonoBehaviour 
 {
+  public GameObject Wall;
+  public GameObject Floor;
+  public GameObject Portcullis;
+  public GameObject Stairs;
+  public GameObject Torch;
+
+  void Awake()
+  {
+    Vector3 cameraPos = Camera.main.transform.position;
+
+    // Walls
+
+    var go = (GameObject)Instantiate(Wall, new Vector3(cameraPos.x - 2, 0, cameraPos.z + 4), Quaternion.identity);
+    go = (GameObject)Instantiate(Wall, new Vector3(cameraPos.x + 2, 0, cameraPos.z + 4), Quaternion.identity);
+    go = (GameObject)Instantiate(Wall, new Vector3(cameraPos.x - 2, 2, cameraPos.z + 4), Quaternion.identity);
+    go = (GameObject)Instantiate(Wall, new Vector3(cameraPos.x + 2, 2, cameraPos.z + 4), Quaternion.identity);
+    go = (GameObject)Instantiate(Wall, new Vector3(cameraPos.x, 2, cameraPos.z + 4), Quaternion.identity);
+
+    // Torches
+
+    go = (GameObject)Instantiate(Torch, new Vector3(cameraPos.x - 1.5f, 0, cameraPos.z + 4), Quaternion.identity);
+    go.transform.Rotate(Vector3.up, 180.0f, Space.World);
+
+    go = (GameObject)Instantiate(Torch, new Vector3(cameraPos.x + 1.5f, 0, cameraPos.z + 4), Quaternion.identity);
+    go.transform.Rotate(Vector3.up, 180.0f, Space.World);
+
+    // Floor and ceiling
+
+    go = (GameObject)Instantiate(Floor, new Vector3(cameraPos.x, 0, cameraPos.z + 4), Quaternion.identity);
+
+    go = (GameObject)Instantiate(Floor, new Vector3(cameraPos.x, 2, cameraPos.z + 4), Quaternion.identity);
+    go.transform.Rotate(Vector3.right, 180.0f, Space.World);
+
+    go = (GameObject)Instantiate(Floor, new Vector3(cameraPos.x - 2, 0, cameraPos.z + 2), Quaternion.identity);
+    go = (GameObject)Instantiate(Floor, new Vector3(cameraPos.x, 0, cameraPos.z + 2), Quaternion.identity);
+    go = (GameObject)Instantiate(Floor, new Vector3(cameraPos.x + 2, 0, cameraPos.z + 2), Quaternion.identity);
+
+    // Portcullis
+
+    go = (GameObject)Instantiate(Portcullis, new Vector3(cameraPos.x, 0, cameraPos.z + 4), Quaternion.identity);
+    go.transform.Rotate(Vector3.up, 180.0f, Space.World);
+
+    // Stairs
+  }
+
   public void NewGameHandler()
   {
     Application.LoadLevel("main");
