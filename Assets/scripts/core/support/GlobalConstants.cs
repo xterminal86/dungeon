bf3cdd74333b2e2b035cef550293ddbe7af172db
xterminal86 <xterminal86@gmail.com>
2017-02-04 01:06:34 +0300

    refs #17 WIP

--- Assets/scripts/core/support/GlobalConstants.cs
+++ Assets/scripts/core/support/GlobalConstants.cs
@@ -447,6 +447,16 @@ public class VillagerInfo
 
 public class BlockEntity
 {
+  public bool Walkable = true;
+  public bool IsLiquid = false;
+  public bool SkipInstantiation = false;
+  public string BlockName = string.Empty;
+  public Vector3 ArrayCoordinates = Vector3.zero;
+  public Vector3 WorldCoordinates = Vector3.zero;
+  public GlobalConstants.FootstepSoundType FootstepSound = GlobalConstants.FootstepSoundType.DUMMY;
+
+  public Dictionary<GlobalConstants.Orientation, bool> SidesWalkability = new Dictionary<GlobalConstants.Orientation, bool>();
+
   GlobalConstants.BlockType _blockType = GlobalConstants.BlockType.AIR;
   public GlobalConstants.BlockType BlockType
   {
@@ -458,20 +468,19 @@ public class BlockEntity
     }
   }
 
-  public string BlockName = string.Empty;
-  public Vector3 ArrayCoordinates = Vector3.zero;
-  public Vector3 WorldCoordinates = Vector3.zero;
-  public bool IsLiquid = false;
-  public bool SkipInstantiation = false;
-  public GlobalConstants.FootstepSoundType FootstepSound = GlobalConstants.FootstepSoundType.DUMMY;
-
   public BlockEntity()
   {
+    Walkable = true;
+
+    SidesWalkability[GlobalConstants.Orientation.EAST] = true;
+    SidesWalkability[GlobalConstants.Orientation.SOUTH] = true;
+    SidesWalkability[GlobalConstants.Orientation.WEST] = true;
+    SidesWalkability[GlobalConstants.Orientation.NORTH] = true;
   }
 
   public void SetFootstepSound()
   {
-    switch (BlockType)
+    switch (_blockType)
     {
       case GlobalConstants.BlockType.GRASS:
         FootstepSound = GlobalConstants.FootstepSoundType.GRASS;
@@ -479,6 +488,14 @@ public class BlockEntity
 
       case GlobalConstants.BlockType.DIRT:
         FootstepSound = GlobalConstants.FootstepSoundType.DIRT;
+        break;
+
+      case GlobalConstants.BlockType.STONE:
+        FootstepSound = GlobalConstants.FootstepSoundType.STONE;
+        break;
+
+      case GlobalConstants.BlockType.WOOD_OAK:
+        FootstepSound = GlobalConstants.FootstepSoundType.WOOD;
         break;
 
       default:        
